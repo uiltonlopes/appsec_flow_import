2d602da01a49daa6bdae84284446aa840a3b81d0
Uilton Lopes <uilton.lopes.moura@gmail.com>
2020-05-27 20:32:46 -0300

    add client

---
+++ lib/appsec_flow_import/client.rb
@@ -0,0 +1,35 @@
+module AppsecFlowImport
+  class Client
+    attr_reader :api_key, :base_url
+
+    def initialize(api_key, environment)
+      @api_key = api_key
+      load_base_url_for(environment)
+    end
+
+    def vulnerabilities
+      AppsecFlowImport::Vulnerability.new(self)
+    end
+
+    def vulnerability_templates
+      AppsecFlowImport::VulnerabilityTemplate.new(self)
+    end
+
+    private
+
+    def load_base_url_for(environment)
+      case environment
+      when 'on_premise'
+        @base_url = 'https://localhost'
+      when 'localhost'
+        @base_url = 'http://localhost:3000'
+      when 'production'
+        @base_url = 'https://app.conviso.com.br'
+      when 'staging'
+        @base_url = 'https://homologa.conviso.com.br'
+      else
+        raise ArgumentError, "environment must be 'production' or 'staging'"
+      end
+    end
+  end
+end
